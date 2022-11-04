605666614f031f72e9ef57b2a2650d52c134d8b2
wileyj <2847772+wileyj@users.noreply.github.com>
2022-11-04 14:10:54 -0700

    use isomorphic-fetch

--- pages/api/refresh.js
+++ pages/api/refresh.js
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import * as fetch from "node-fetch";
+import * as fetch from "isomorphic-fetch";
 import prisma from '../../lib/db';
 
 export default async function handler(req,res) {
