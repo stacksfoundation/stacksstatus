49083bc7ba129da1fae51c0f0eb389157450e6ab
wileyj <2847772+wileyj@users.noreply.github.com>
2022-11-04 15:38:34 -0700

    update queries

--- pages/index.js
+++ pages/index.js
@@ -15,14 +15,14 @@ import { convertEpoch } from "../lib/util";
 const Home = (props) => {
   if (process.env.NODE_ENV != 'production') {
     console.log("mempool_size: " + props.mempool_size.data);
-    console.log("block_time: " + props.block_time.data);
-    console.log("block_txs: " + props.block_txs.data);
-    console.log("block_height: " + props.blocks.block_height);
-    console.log("block_hash: " + props.blocks.block_hash);
-    console.log("burn_block_time: " + props.blocks.burn_block_time);
-    // console.log("single_tx_blocks:" + props.single_tx_blocks.data); // disabled
-    console.log("tx_fees_daily: " + props.tx_fees_daily.data)
-    console.log("tx_fees_hourly: " + props.tx_fees_hourly.data)
+    // console.log("block_time: " + props.block_time.data);
+    // console.log("block_txs: " + props.block_txs.data);
+    // console.log("block_height: " + props.blocks.block_height);
+    // console.log("block_hash: " + props.blocks.block_hash);
+    // console.log("burn_block_time: " + props.blocks.burn_block_time);
+    // // console.log("single_tx_blocks:" + props.single_tx_blocks.data); // disabled
+    // console.log("tx_fees_daily: " + props.tx_fees_daily.data)
+    // console.log("tx_fees_hourly: " + props.tx_fees_hourly.data)
     console.log("node_env: " + process.env.NODE_ENV)
   }
   
@@ -249,59 +249,64 @@ export const getServerSideProps = async () => {
 
   let block_txs;
   try {
-    block_txs = await prisma.block_txs.findFirst({
-      orderBy: {
-        ts: "desc",
-      },
-    });
+    // block_txs = await prisma.block_txs.findFirst({
+    //   orderBy: {
+    //     ts: "desc",
+    //   },
+    // });
+    block_txs = await prisma.$queryRawUnsafe(`select * from block_txs order by ts desc limit 1;`);
   } catch {
     block_txs = { data: "Error fetching tx data" };
   }
 
   let blocks;
   try {
-    blocks = await prisma.blocks.findFirst({
-      orderBy: {
-        ts: "desc",
-      },
-    });
+    // blocks = await prisma.blocks.findFirst({
+    //   orderBy: {
+    //     ts: "desc",
+    //   },
+    // });
+    blocks = await prisma.$queryRawUnsafe(`select * from blocks order by ts desc limit 1;`);
   } catch {
     blocks = { data: "Error fetching last block data" };
   }
 
   let mempool_size;
   try {
-    mempool_size = await prisma.mempool_size.findFirst({
-      orderBy: {
-        ts: "desc",
-      },
-    });
+    // mempool_size = await prisma.mempool_size.findFirst({
+    //   orderBy: {
+    //     ts: "desc",
+    //   },
+    // });
+    mempool_size = await prisma.$queryRawUnsafe(`select * from mempool_size order by ts desc limit 1;`);
   } catch (e) {
+      console.log("error: "+ e)
     mempool_size = { data: "Error fetching mempool data" };
   }
 
   // // Disabled 
   // let single_tx_blocks;
   // try {
-  //   single_tx_blocks = await prisma.single_tx_blocks.findFirst({
-  //     orderBy: {
-  //       ts: "desc",
-  //     },
-  //   });
+  //   // single_tx_blocks = await prisma.single_tx_blocks.findFirst({
+  //   //   orderBy: {
+  //   //     ts: "desc",
+  //   //   },
+  //   // });
+  //   single_tx_blocks = await prisma.$queryRawUnsafe(`select * from single_tx_blocks order by ts desc limit 1;`);
   // } catch {
   //   single_tx_blocks = { data: "Error fetching block tx data" };
   // }
 
   let tx_fees_daily;
   try {
-    tx_fees_daily = await prisma.$queryRawUnsafe(`select data from tx_fees_daily order by ts desc limit 1;`);
+    tx_fees_daily = await prisma.$queryRawUnsafe(`select * from tx_fees_daily order by ts desc limit 1;`);
   } catch {
     tx_fees_daily = { data: "Error fetching daily fee data" };
   }
 
   let tx_fees_hourly;
   try {
-    tx_fees_hourly = await prisma.$queryRawUnsafe(`select data from tx_fees_hourly order by ts desc limit 1;`);
+    tx_fees_hourly = await prisma.$queryRawUnsafe(`select * from tx_fees_hourly order by ts desc limit 1;`);
   } catch {
     tx_fees_hourly = { data: "Error fetching hourly fee data" };
   }
@@ -325,10 +330,10 @@ export const getServerSideProps = async () => {
   }
   return {
     props: {
-      mempool_size: makeSerializable(mempool_size),
+      mempool_size: makeSerializable(mempool_size[0]),
       block_time: makeSerializable(block_time[0]),
-      block_txs: makeSerializable(block_txs),
-      blocks: makeSerializable(blocks),
+      block_txs: makeSerializable(block_txs[0]),
+      blocks: makeSerializable(blocks[0]),
       // single_tx_blocks: makeSerializable(single_tx_blocks),  // Disabled
       tx_fees_daily: makeSerializable(tx_fees_daily[0]),
       tx_fees_hourly: makeSerializable(tx_fees_hourly[0]),
