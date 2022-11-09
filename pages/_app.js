28315df7c32d4e5a8167b789766722c2dcfdfc81
wileyj <2847772+wileyj@users.noreply.github.com>
2022-11-08 19:50:06 -0800

    fix linting

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,21 @@
-import Head from 'next/head';
-import '../styles/globals.css';
+import Head from 'next/head'
+import '../styles/globals.css'
+import React from 'react'
 
-
-function MyApp({ Component, pageProps: { ...pageProps} }) {
-	return (
-		<>
-			<Head>
-				<title>Stacks Blockchain Status</title>
-				<meta name="description" content="Stacks Blockchain Status" />
-				<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-				<meta property="title" content="Stacks Blockchain Status" key="title" />
-				<meta name="theme-color" content="#05030A" />
-				<meta property="description" content="Stacks Blockchain Status" key="description" />
-			</Head>
+function MyApp ({ Component, pageProps: { ...pageProps } }) {
+  return (
+    <>
+      <Head>
+        <title>Stacks Blockchain Status</title>
+        <meta name="description" content="Stacks Blockchain Status" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="title" content="Stacks Blockchain Status" key="title" />
+        <meta name="theme-color" content="#05030A" />
+        <meta property="description" content="Stacks Blockchain Status" key="description" />
+      </Head>
       <Component {...pageProps} />
-		</>
-	);
+    </>
+  )
 }
 
 export default MyApp
