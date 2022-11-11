73e940f8e8a4be55d5146e7df0e25c101c62a9cd
wileyj <2847772+wileyj@users.noreply.github.com>
2022-11-11 11:04:26 -0800

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ function MyApp ({ Component, pageProps: { ...pageProps } }) {
         <meta property="title" content="Stacks Blockchain Status" key="title" />
         <meta name="theme-color" content="#05030A" />
         <meta property="description" content="Stacks Blockchain Status" key="description" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Component {...pageProps} />
     </>
