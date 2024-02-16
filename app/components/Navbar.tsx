89a2021ada7cc4d479e419aabe526692f5a1cc11
cbadawi <cb_1974@protonmail.com>
2024-02-16 09:14:58 +0530

    chore : minor navbar ui improvement

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,11 +3,8 @@ import Fees from './Fees/Fees';
 
 const Navbar = () => {
   return (
-    <div className='navbar flex justify-between py-2'>
-      <Link
-        href='/'
-        className='btn btn-ghost ml-5 text-xl md:text-xl xl:text-xl'
-      >
+    <div className='navbar flex h-20 items-center  justify-between py-2'>
+      <Link href='/' className='ml-5 text-xl md:text-xl xl:text-xl'>
         Stacks Status
       </Link>
       <Fees />
