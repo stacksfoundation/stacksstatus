52909a146714ddc502dd1178149d5bf245be98fe
cbadawi <cb_1974@protonmail.com>
2024-02-16 09:16:11 +0530

    chore : upgrade to ts

---
+++ lib/util.ts
@@ -0,0 +1,28 @@
+export const apiRoot = 'https://api.hiro.so';
+
+export const millisecondsPerHour = 60 * 60 * 1000;
+export const millisecondsPerDay = 24 * millisecondsPerHour;
+
+export function makeSerializable(o) {
+  return JSON.parse(JSON.stringify(o));
+}
+
+export const getTimestampFromNow = (milliseconds: number) => {
+  const now = new Date();
+  const timestampDaysAgo = new Date(now.getTime() - milliseconds).getTime();
+  return timestampDaysAgo;
+};
+
+export function convertEpoch(date) {
+  const myDate = new Date(date * 1000);
+  return myDate.toUTCString();
+}
+
+export const isProd = process.env.NODE_ENV === 'production';
+
+// pages/api/user
+export async function getData(url) {
+  const response = await fetch(url, { next: { revalidate: isProd ? 90 : 0 } });
+  const jsonData = await response.json();
+  return jsonData;
+}
