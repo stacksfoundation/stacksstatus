4d1565925c07d205a5c017500b4cd2dd6f5c2ef9
cbadawi <cb_1974@protonmail.com>
2024-02-29 07:51:07 +0200

    add footer

--- app/components/Navigation/Footer.tsx
+++ app/components/Navigation/Footer.tsx
@@ -1,7 +1,69 @@
+'use server';
+
 import React from 'react';
+import Image from 'next/image';
 
 const Footer = () => {
-  return <div className='w-full bg-[#141414]'>Footer todo</div>;
+  return (
+    <div className='flex w-full items-center justify-center bg-[#141414] p-2'>
+      <a
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://stacks.org'
+        className='mx-2'
+      >
+        <Image
+          priority
+          src={'/images/stacks.svg'}
+          height={28}
+          width={28}
+          alt='stacks'
+        />
+      </a>
+      <a
+        className='mx-2'
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://github.com/stacksfoundation/stacksstatus'
+      >
+        <Image
+          priority
+          src={'/images/github.svg'}
+          height={28}
+          width={28}
+          alt='stacks'
+        />
+      </a>
+      <a
+        className='mx-2'
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://discord.gg/HsK3ShU'
+      >
+        <Image
+          priority
+          src={'/images/discord.svg'}
+          height={28}
+          width={28}
+          alt='stacks'
+        />
+      </a>
+      <a
+        className='mx-2'
+        target='_blank'
+        rel='noopener noreferrer'
+        href='https://twitter.com/stacksstatus'
+      >
+        <Image
+          priority
+          src={'/images/twitter.svg'}
+          height={28}
+          width={28}
+          alt='stacks'
+        />
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
