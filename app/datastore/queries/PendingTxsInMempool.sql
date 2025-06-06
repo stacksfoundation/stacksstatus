6b68e090d837c3cf8a42fb2fd53fd7c778c3da30
wileyj <2847772+wileyj@users.noreply.github.com>
2025-06-06 10:19:33 -0700

    use 1 hour interval for data

--- app/datastore/queries/PendingTxsInMempool.sql
+++ app/datastore/queries/PendingTxsInMempool.sql
@@ -1,1 +1,1 @@
-select date, size from stacks_blockchain_api.mempool_size where date >= now()- INTERVAL '3 HOURS';
+select date, size from stacks_blockchain_api.mempool_size where date >= now()- INTERVAL '1 HOURS';
--- app/datastore/queries/TxsPerBlock.sql
+++ app/datastore/queries/TxsPerBlock.sql
@@ -1,5 +1,5 @@
 select block_height, tx_count
 from blocks
 where canonical 
-and burn_block_time>= EXTRACT(EPOCH FROM NOW() - INTERVAL '3 HOURS')::INTEGER
+and burn_block_time>= EXTRACT(EPOCH FROM NOW() - INTERVAL '1 HOURS')::INTEGER
 order by 1;
